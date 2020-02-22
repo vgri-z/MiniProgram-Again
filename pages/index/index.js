d6e82f39661c6c629379de3eba8b4ddcd35cc251
vgri-z <vgri199712@163.com>
2020-02-22 17:42:29 +0800

    网络请求的封装

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,64 @@
 // pages/index/index.js
+import request from "../../services/network.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
+  onLoad: function(options) {
+    // 原生的方式发送网络请求
+    this.get_data_origin();
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    // 2.使用封装的request发送网络请求
+    // Promise最大的好处就是防止出现回调地狱
+    // request({
+    //   url: "http://106.54.54.237:8000/api/hy/recommend",
+    // }).then(res => {
+    //   console.log(res)
+    // })
 
+    request({
+      url: "http://106.54.54.237:8000/api/hy/home/data",
+      data: {
+        type: "sell",
+        page: 1
+      }
+    }).then(res => {
+      console.log(res)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  get_data_origin() {
+    // 1. 发送最简单的get请求
+    // wx:wx.request({
+    //   url: "http://106.54.54.237:8000/api/hy/recommend",
+    //   success: (res) => {
+    //     console.log(res)
+    //   }
+    // })
+    // 2.get请求，并且携带参数
+    // wx: wx.request({
+    //   url: "http://106.54.54.237:8000/api/hy/home/data",
+    //   data: {
+    //     type: "sell",
+    //     page: 1
+    //   },
+    //   success: (res) => {
+    //     console.log(res)
+    //   }
+    // })
+    // 3.post请求，并且携带参数
+    // wx:wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method: "post",
+    //   data: {
+    //     name: "vae",
+    //     age: 32
+    //   },
+    //   success: (res) => {
+    //     console.log(res)
+    //   },
+    //   fali: (err) => {
+    //     console.log(err)
+    //   }
+    // })
   }
 })
